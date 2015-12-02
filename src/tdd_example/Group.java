0058fd341c3000c26bc5e6ed394c125d14ae60e4
Alexandre Terrasa <alexandre@moz-code.org>
2015-12-02 09:43:28 -0500

    group: Add mail sending service based on MailSender interface.
    Signed-off-by: Alexandre Terrasa <alexandre@moz-code.org>

--- src/tdd_example/Group.java
+++ src/tdd_example/Group.java
@@ -51,4 +51,11 @@ public class Group {
         }
         return sum / students.size();
     }
+
+    public void sendGroupMail(String subject, String message) {
+        MailSender mailSender = new UQAMMailSender();
+        for(Student student : students.values()) {
+            mailSender.send(student.getPermaCode(), subject, message);
+        }
+    }
 }
